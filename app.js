a08bb33062ef48b6a1b811b76617d61fa0a99d7b
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-10 14:56:01 -0300

    Adicionada a biblioteca CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 const morgan = require('morgan')
@@ -6,13 +7,14 @@ const morgan = require('morgan')
 app.use(morgan('dev'))
 app.use(express.urlencoded({extend: false}))
 app.use(express.json())
+app.use(cors())
 
 const rotaTodos = require('./routes/todos')
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header(
-        'Access-Control-Allow-Header',
+        'Access-Control-Allow-Headers',
         'Origin, X-Request-With, Content-Type, Accept, Authorization'
     )
 
